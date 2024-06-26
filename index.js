6dfb70b4e23c734d3d36d4022f2e9107855f30f4
AraibIrfan <araibirfan09@gmail.com>
2024-06-26 20:08:16 +0500

    many changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import pg from 'pg'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import bodyparser from 'body-parser';
 dotenv.config()
 
 const app = express()
@@ -16,14 +16,16 @@ const db = new pg.Client({
     password: process.env.PSQL_PASSWORD,
     port: process.env.PSQL_PORT,
     connectionString: process.env.PSQL_DATABASE_URL,
-    ssl:true
+    ssl: true
 })
 
 db.connect()
 app.use(cors());
+app.use(express.json())
+app.use(bodyparser.json())
 app.get('/', async (req, res) => {
     try {
-        const response = await db.query('SELECT author_name,book_name,description,rating FROM book JOIN author ON author.author_id = book.author_id ORDER BY rating DESC')
+        const response = await db.query('SELECT book_id,author_name,book_name,description,rating FROM book JOIN author ON author.author_id = book.author_id ORDER BY rating DESC')
         const result = response.rows
         res.json(result)
         console.log(result)
@@ -32,17 +34,17 @@ app.get('/', async (req, res) => {
     }
 })
 
-app.get("/api-sortbyauthor",async (req,res)=>{
-   try {
-    const response = await db.query('SELECT author_name,book_name,description,rating FROM book JOIN author ON author.author_id = book.author_id ORDER BY author_name ASC')
-    const result = response.rows
-    res.json(result)
-   } catch (error) {
-    console.error(error)
-   }
+app.get("/api-sortbyauthor", async (req, res) => {
+    try {
+        const response = await db.query('SELECT author_name,book_name,description,rating FROM book JOIN author ON author.author_id = book.author_id ORDER BY author_name ASC')
+        const result = response.rows
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+    }
 })
 
-app.get("/api-sortbytitle",async (req,res)=>{
+app.get("/api-sortbytitle", async (req, res) => {
     try {
         const response = await db.query('SELECT author_name,book_name,description,rating FROM book JOIN author ON author.author_id = book.author_id ORDER BY book_name ASC')
         const result = response.rows
@@ -50,6 +52,35 @@ app.get("/api-sortbytitle",async (req,res)=>{
     } catch (error) {
         console.error(error)
     }
+})
+
+app.post("/bookdata", async (req, res) => {
+    try {
+        const response = req.body
+        const author_name = response.data.authorName
+        const book_name = response.data.bookName
+        const rating = response.data.rating
+        const description = response.data.description
+
+        const authorResult = await db.query('SELECT author_id from author WHERE author_name = $1',[author_name])
+        let authorId;
+        if(authorResult.rows.length === 0){
+           const newAuthorResult =  await db.query('INSERT INTO author (author_name) VALUES ($1) RETURNING author_id',[author_name])
+            authorId = newAuthorResult.rows[0].author_id
+        }
+        else{
+            authorId = authorResult.rows[0].author_id
+        }
+       
+        await db.query('INSERT INTO book (book_name,description,rating,author_id) VALUES ($1, $2, $3, $4)',[book_name,description,rating,authorId])
+        res.redirect("/")
+    } catch (error) {
+        console.log(error)
+    }
+})
+app.post('/notes',(req,res)=>{
+    const response = req.body
+    console.log(response)
 })
 app.listen(port, () => {
 
